80ce285273bd31e07143299b3297b94590673279
sys-dev-me <44780251+sys-dev-me@users.noreply.github.com>
2018-11-20 13:56:43 +0200

    Update main func

--- token2hex.go
+++ token2hex.go
@@ -1,16 +1,10 @@
 package main
 
 import "encoding/hex"
-//import "encoding/base64"
 import "fmt"
 import "os"
 
 func main () {
-//	data, err := base64.StdEncoding.DecodeString ( os.Args[1] )
-//	if err !=nil {
-//		fmt.Println ( "Errr string" )
-//	}
-//	fmt.Println ( data )
 	src := []byte( os.Args[1]  )
 	fmt.Println( hex.EncodeToString( src))
 }
